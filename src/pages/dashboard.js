fa4ebaec7ae9b776e0b37bb5ea6f21193dc2ac2f
GeorgiStavrev <georgi@insurify.com>
2021-04-10 20:12:14 +0300

    sends more complex request structure

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -12,51 +12,61 @@ import axios from "axios";
 
 function DashboardPage(props) {
   const defaultPlaybook = {
-    id:1,
-    name:"test playbook",
+    id: 1,
+    name: "test playbook",
     workflow: {
-      payload: {
-        request: {
-          options: {
+      steps: [
+        {
+          name: "Call google.com",
+          type: "http_get",
+          requestOptions: {
             hostname: "www.google.com",
             port: 80,
-            protocol: "http:"
-          }
-        }
-      }
-    }
-  }
+            protocol: "http",
+            path: "",
+          },
+          users: 1,
+          repeats: 10,
+          wait: 5,
+        },
+      ],
+    },
+  };
   const msgSuccess = "Great success!";
   const msgFailure = "Oh no!";
   const [playbook, setPlaybook] = useState(defaultPlaybook);
   const [toastShown, setToastShown] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
-  
-  const runPlaybook = () => {    
+
+  const runPlaybook = () => {
     makeRequest(
-      playbook.workflow.payload,
-      response => {
+      playbook.workflow,
+      (response) => {
         console.log("Successfully ran playbook!");
         setToastMessage(msgSuccess);
         setToastShown(true);
       },
-      err => {
+      (err) => {
         console.log("Failure to run playbook", err);
         setToastMessage(msgFailure);
         setToastShown(true);
       }
     );
-  }
+  };
 
   const makeRequest = (postData, onSuccess, onError) => {
-    axios.post("https://l9sbd4f7s9.execute-api.eu-west-1.amazonaws.com/v1_1/loadtest", postData)
-    .then(response => onSuccess(response))
-    .catch(error => onError(error));
-  }
+    axios
+      .post(
+        "https://l9sbd4f7s9.execute-api.eu-west-1.amazonaws.com/v1_1/loadtest",
+        postData
+      )
+      .then((response) => onSuccess(response))
+      .catch((error) => onError(error));
+  };
 
   const loadPlaybook = () => {
-    alert('Still under construction');
-  }
+    alert("Still under construction");
+  };
   return (
     <Section
       bg={props.bg}
@@ -73,28 +83,45 @@ function DashboardPage(props) {
           spaced={true}
           className="text-center"
         />
-        <Toast onClose={() => setToastShown(false)} show={toastShown} delay={3000} autohide>
+        <Toast
+          onClose={() => setToastShown(false)}
+          show={toastShown}
+          delay={3000}
+          autohide
+        >
           <Toast.Body>{toastMessage}</Toast.Body>
         </Toast>
         <Row>
-          <Col lg={6}>            
+          <Col lg={6}>
             <Card>
-                <Card.Body>
-                  <h5 className="mb-3">Playbook: {playbook.name}</h5>
-                  <Button variant="primary" onClick={runPlaybook}>Start load test </Button>
-                </Card.Body>
-              </Card>
+              <Card.Body>
+                <h5 className="mb-3">Playbook: {playbook.name}</h5>
+                <Button variant="primary" onClick={runPlaybook}>
+                  Start load test{" "}
+                </Button>
+              </Card.Body>
+            </Card>
           </Col>
           <Col lg={6} className="mt-4 mt-lg-0">
             <Card>
               <Card.Body>
                 <h5 className="mb-3">Available load test playbooks</h5>
                 <ListGroup>
-                  <ListGroup.Item action onClick={loadPlaybook}>Cras justo odio</ListGroup.Item>
-                  <ListGroup.Item action onClick={loadPlaybook}>Dapibus ac facilisis in</ListGroup.Item>
-                  <ListGroup.Item action onClick={loadPlaybook}>Morbi leo risus</ListGroup.Item>
-                  <ListGroup.Item action onClick={loadPlaybook}>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item action onClick={loadPlaybook}>Vestibulum at eros</ListGroup.Item>
+                  <ListGroup.Item action onClick={loadPlaybook}>
+                    Cras justo odio
+                  </ListGroup.Item>
+                  <ListGroup.Item action onClick={loadPlaybook}>
+                    Dapibus ac facilisis in
+                  </ListGroup.Item>
+                  <ListGroup.Item action onClick={loadPlaybook}>
+                    Morbi leo risus
+                  </ListGroup.Item>
+                  <ListGroup.Item action onClick={loadPlaybook}>
+                    Porta ac consectetur ac
+                  </ListGroup.Item>
+                  <ListGroup.Item action onClick={loadPlaybook}>
+                    Vestibulum at eros
+                  </ListGroup.Item>
                 </ListGroup>
               </Card.Body>
             </Card>
