427a9b0d1307e58ce75b1465267af120c4661f51
GeorgiStavrev <georgi@insurify.com>
2021-09-04 16:27:07 +0300

    adds 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Section from "components/Section";
+import { Container, Row, Col } from "react-bootstrap";
+import "pages/404.scss";
+
+function Page404() {
+  return (
+    <Section>
+      <Container>
+        <Row>
+          <Col className="col-vcenter">
+            <img
+              src="/images/illustrations/not_found.svg"
+              alt="not found cat in a box"
+              width="640"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="col-vcenter">
+            <h1>Nothing to see here.</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="col-vcenter">
+            <h2>404 Page Not Found</h2>
+          </Col>
+        </Row>
+      </Container>
+    </Section>
+  );
+}
+
+export default Page404;
