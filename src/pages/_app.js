65baf7b2866e651ede523291ab1c2d151b418c69
GeorgiStavrev <georgi@insurify.com>
2021-09-11 11:50:26 +0300

    change ico

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,34 +3,43 @@ import "styles/global.scss";
 import NavbarCustom from "components/NavbarCustom";
 import Footer from "components/Footer";
 import "util/analytics.js";
+import Head from "next/head";
 import { AuthProvider } from "util/auth.js";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <>
-        <NavbarCustom
-          signoutRoute="/"
-          bg="white"
-          variant="light"
-          expand="md"
-          logo="/images/logo.svg"
-        />
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/images/logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </Head>
+      <AuthProvider>
+        <>
+          <NavbarCustom
+            signoutRoute="/"
+            bg="white"
+            variant="light"
+            expand="md"
+            logo="/images/logo.svg"
+          />
 
-        <Component {...pageProps} />
+          <Component {...pageProps} />
 
-        <Footer
-          bg="light"
-          textColor="dark"
-          size="sm"
-          bgImage=""
-          bgImageOpacity={1}
-          description="A short description of what you do here"
-          copyright="© 2021 moggies.io"
-          logo="/images/logo.svg"
-        />
-      </>
-    </AuthProvider>
+          <Footer
+            bg="light"
+            textColor="dark"
+            size="sm"
+            bgImage=""
+            bgImageOpacity={1}
+            description="A short description of what you do here"
+            copyright="© 2021 moggies.io"
+            logo="/images/logo.svg"
+          />
+        </>
+      </AuthProvider>
+    </>
   );
 }
 
